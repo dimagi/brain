44e98a56cb77ecad46a067c44e7996634c79f23e
Ben <rudolphben@gmail.com>
2016-09-08 17:06:23 -0400

    better relevance tools

--- learn/m2m.py
+++ learn/m2m.py
@@ -1,5 +1,6 @@
 import numpy
 import random
+from collections import defaultdict
 from sklearn import svm, cross_validation, linear_model, metrics
 from matplotlib import pyplot as plt
 from ggplot import qplot
@@ -8,14 +9,13 @@ from normalize import utils
 
 class M2MLearn(object):
 
-    def __init__(self, gamma=0.001, C=100):
+    def __init__(self, gamma=0.001, C=1000):
         self.gamma = gamma  # aproximate optimial is 0.001
         self.C = C  # Optmial is 100
         self.model = None
 
     def fit(self, data, target):
-        self.model = svm.SVC(gamma=self.gamma, C=self.C, probability=True)
-        self.model.fit(data, target)
+        raise NotImplementedError()
 
     def relevance(self, dataset, column):
         regression = linear_model.LinearRegression()
@@ -34,14 +34,92 @@ class M2MLearn(object):
             test_column,
             test_targets,
         )
-        plt.scatter(
-            numpy.array(range(len(test_column))),
-            utils.jitter(test_column),
-            c=test_targets,
-            cmap=plt.cm.Paired
+
+        # Graph feature scattered with red dots that indicate LTFU
+        # plt.scatter(
+        #     numpy.array(range(len(test_column))),
+        #     utils.jitter(test_column),
+        #     c=test_targets,
+        #     cmap=plt.cm.Paired
+        # )
+        # plt.show()
+
+        values = self._get_bucket_percentages(dataset.train, dataset.train_targets)
+        feature_labels = map(lambda value: value[0], values)
+        percentages = map(lambda value: value[1], values)
+        plt.bar(
+            feature_labels,
+            percentages,
         )
+        plt.title(column)
+        plt.axis([0, len(feature_labels), 0, 1])
         plt.show()
+
         return test_column, test_targets
+
+    def _get_bucket_percentages(self, train, targets):
+        EPSILON = 0.01
+        buckets = defaultdict(lambda: 0)
+        for idx, row in enumerate(train):
+            assert len(row) == 1
+            buckets[row[0]] += 1
+
+        for key, count in buckets.iteritems():
+            buckets[key] = (key, buckets[key] / float(len(targets)))
+
+        assert 1.0 - EPSILON <= sum(map(lambda v: v[1], buckets.values())) <= 1.0 + EPSILON
+        return buckets.values()
+
+    def predict(self, datum):
+        if not self.model:
+            raise Exception('Must train model before predicting')
+
+        return self.model.predict_proba(datum.astype('float'))
+
+    def roc_curve(self, probabilitites, target):
+        return metrics.roc_curve(
+            target[:, 0],
+            probabilitites[:, 0],
+            pos_label='1',
+        )
+
+    def score_probs(self, y_true, y_prob):
+        return metrics.brier_score_loss(y_true, y_prob)
+
+    def score(self, datum, target):
+        if not self.model:
+            raise Exception('Must train model before predicting')
+
+        return self.model.score(datum, target)
+
+
+class M2MLearnLogisticRegression(M2MLearn):
+
+    def fit(self, data, target):
+        self.model = linear_model.LogisticRegressionCV(
+            Cs=self.C,
+            scoring=utils.brier_scorer,
+            penalty='l2',
+            solver='liblinear',
+            refit=True,
+            n_jobs=-1,
+        )
+        self.model.fit(data, target)
+
+    def cross_validate(self, data, target):
+        pass
+
+
+class M2MLearnSvm(M2MLearn):
+
+    def fit(self, data, target):
+        self.model = svm.SVC(
+            gamma=self.gamma,
+            C=self.C,
+            class_weight={'0': 0.9, '1': 0.1},
+            probability=True,
+        )
+        self.model.fit(data, target)
 
     def cross_validate(self, data, target):
         gamma = [0.0001, 0.001, 0.01, 0.1, 1]
@@ -60,28 +138,6 @@ class M2MLearn(object):
                 tmp_model, data, target[:, 0], cv=5, scoring=utils.brier_scorer
             )
             print("Accuracy for %s: %0.2f (+/- %0.2f)" % (str(c_val), scores.mean(), scores.std() * 2))
-
-    def predict(self, datum):
-        if not self.model:
-            raise Exception('Must train model before predicting')
-
-        return self.model.predict_proba(datum)
-
-    def roc_curve(self, probabilitites, target):
-        return metrics.roc_curve(
-            target[:, 0],
-            probabilitites[:, 0],
-            pos_label='1',
-        )
-
-    def score_probs(self, y_true, y_prob):
-        return metrics.brier_score_loss(y_true, y_prob)
-
-    def score(self, datum, target):
-        if not self.model:
-            raise Exception('Must train model before predicting')
-
-        return self.model.score(datum, target)
 
 
 class DummyM2MLearn(object):
@@ -106,5 +162,5 @@ class DummyM2MLearn(object):
         return metrics.brier_score_loss(y_true, y_prob)
 
     def predict(self, datum):
-        predictions = map(lambda _: [1, 0], datum)
+        predictions = map(lambda _: [1 - self.model, self.model], datum)
         return numpy.array(predictions)
